08d2228479949af9592baa1992630ae4a1a40f3b
Kamil Skalski <kamil.skalski@gmail.com>
2019-05-02 11:30:42 +0200

    Initial version.

---
+++ ProtoGardenEF/ProtoGardenEF/Database.cs
@@ -0,0 +1,46 @@
+﻿using Google.Protobuf;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace ProtoGardenEF {
+  internal class Database : DbContext
+  {
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
+      optionsBuilder.UseSqlite("Data Source=garden.db");
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder) {
+      modelBuilder.Entity<Models.Fountain>()
+        .Property(m => m.SerialNr)
+        .HasConversion(v => v.ToByteArray(), v => ByteString.CopyFrom(v));
+      modelBuilder.Entity<Models.Fountain>()
+        .Property(m => m.LastRun)
+        .HasConversion(v => v.ToDateTimeOffset(), v => Google.Protobuf.WellKnownTypes.Timestamp.FromDateTimeOffset(v));
+      modelBuilder.Entity<Models.Fountain>()
+        .Property(m => m.RunFor)
+        .HasConversion(v => v.ToTimeSpan(), v => Google.Protobuf.WellKnownTypes.Duration.FromTimeSpan(v));
+
+      /* 
+       * I would love to use map<int64, Garden>, which generates a MapField<long, Gargen> as
+       * navigatonal property, where Garden <- Flower relationship is represenced using Flowers' ids.
+       * But it's not possible at the moment.
+       * 
+         modelBuilder.Entity<Models.Garden>()
+        .Property(m => m.Flowers)
+        .HasConversion(v => v.Values, v => v.ToDictionary(flower => flower.Id, flower => flower));
+        */
+    }
+
+    public DbSet<Models.Fruit> Fruits { get; set; }
+    public DbSet<Models.Tree> Trees { get; set; }
+
+    public DbSet<Models.Garden> Gardens { get; set; }
+
+    public DbSet<Models.Fountain> Fountains { get; set; }
+
+    public DbSet<Models.Flower> Flowers { get; set; }
+  }
+}
---
+++ ProtoGardenEF/ProtoGardenEF/Migrations/20190502091505_Initial.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ProtoGardenEF.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Flowers",
+                columns: table => new
+                {
+                    Id = table.Column<long>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Smell = table.Column<string>(nullable: true),
+                    Color = table.Column<int>(nullable: false),
+                    IsBlooming = table.Column<bool>(nullable: false),
+                    NumPetals = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Flowers", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Fountains",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    SerialNr = table.Column<byte[]>(nullable: true),
+                    LastRun = table.Column<DateTimeOffset>(nullable: true),
+                    RunFor = table.Column<TimeSpan>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Fountains", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Gardens",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    FountainId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Gardens", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Gardens_Fountains_FountainId",
+                        column: x => x.FountainId,
+                        principalTable: "Fountains",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Trees",
+                columns: table => new
+                {
+                    Id = table.Column<long>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Height = table.Column<float>(nullable: false),
+                    Age = table.Column<int>(nullable: false),
+                    GardenId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Trees", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Trees_Gardens_GardenId",
+                        column: x => x.GardenId,
+                        principalTable: "Gardens",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Fruits",
+                columns: table => new
+                {
+                    Id = table.Column<long>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Weight = table.Column<double>(nullable: false),
+                    Name = table.Column<string>(nullable: true),
+                    NumSeeds = table.Column<int>(nullable: false),
+                    Taste = table.Column<int>(nullable: false),
+                    TreeId = table.Column<long>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Fruits", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Fruits_Trees_TreeId",
+                        column: x => x.TreeId,
+                        principalTable: "Trees",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Fruits_TreeId",
+                table: "Fruits",
+                column: "TreeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Gardens_FountainId",
+                table: "Gardens",
+                column: "FountainId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Trees_GardenId",
+                table: "Trees",
+                column: "GardenId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Flowers");
+
+            migrationBuilder.DropTable(
+                name: "Fruits");
+
+            migrationBuilder.DropTable(
+                name: "Trees");
+
+            migrationBuilder.DropTable(
+                name: "Gardens");
+
+            migrationBuilder.DropTable(
+                name: "Fountains");
+        }
+    }
+}
