e3b9ac609c3338d8c9affb63f1e90285956fe92a
Kamil Skalski <kamil.skalski@gmail.com>
2019-05-24 21:10:28 +0200

    Fix path with spaces

--- GrpcJsElectronSharp/build.sh
+++ GrpcJsElectronSharp/build.sh
@@ -2,7 +2,7 @@
 
 NODE_EXE=/mnt/d/rep/Kogut/packages/nodejsandnpm/10.0.3/node.exe
 NPM="${NODE_EXE} /rep/Kogut/packages/nodejsandnpm/10.0.3/node_modules/npm/bin/npm-cli.js"
-MSBUILD=/mnt/c/Program\ Files\ \(x86\)/Microsoft\ Visual\ Studio/2017/*/MSBuild/*/Bin/MSBuild.exe
+MSBUILD=/mnt/c/Program*Files*x86*/Microsoft*Visual*Studio/2017/*/MSBuild/*/Bin/MSBuild.exe
 
 ${NPM} install --target-arch=ia32 --arch=ia32 --scripts-prepend-node-path=true --cwd Client --prefix Client
 ${NODE_EXE} Client/node_modules/protobufjs/bin/pbjs -t static-module -w commonjs -o Client/protos.js protos/*.proto
