fb176d15f6b82ef4a95b1467ecca579b4cd0ed7c
Kamil Skalski <kamil.skalski@gmail.com>
2021-10-07 14:56:10 +0200

    Use standard node.exe and npm.cmd

--- GrpcJsElectronSharp/build.sh
+++ GrpcJsElectronSharp/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-NODE_EXE=/mnt/d/rep/Kogut/packages/nodejsandnpm/10.0.3/node.exe
-NPM="${NODE_EXE} /rep/Kogut/packages/nodejsandnpm/10.0.3/node_modules/npm/bin/npm-cli.js"
+NODE_EXE=node.exe
+NPM="cmd.exe /C npm.cmd"
 MSBUILD=/mnt/c/Program*Files*x86*/Microsoft*Visual*Studio/2019/*/MSBuild/*/Bin/MSBuild.exe
 
 ${NPM} install --target-arch=ia32 --arch=ia32 --scripts-prepend-node-path=true --no-bin-links --cwd Client --prefix Client
