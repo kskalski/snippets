1fa47fd1bdc7f4adafdfeea9f2012f3e7c3c9667
Kamil Skalski <kamil.skalski@gmail.com>
2022-03-05 08:30:20 -0500

    Add ignores and prevent generating scripts in npm install for GrpcJsElectronSharp.

--- GrpcJsElectronSharp/build.sh
+++ GrpcJsElectronSharp/build.sh
@@ -4,7 +4,7 @@ NODE_EXE=node.exe
 NPM="cmd.exe /C npm.cmd"
 MSBUILD=/mnt/c/Program*Files*/Microsoft*Visual*Studio/2022/*/MSBuild/*/Bin/MSBuild.exe
 
-${NPM} install --target-arch=ia32 --arch=ia32 --scripts-prepend-node-path=true --no-bin-links --cwd Client --prefix Client
+${NPM} install --target-arch=ia32 --arch=ia32 --scripts-prepend-node-path=true --ignore-scripts --no-bin-links --cwd Client --prefix Client
 
 ${MSBUILD} /restore GrpcJsElectronSharp.sln 
 
