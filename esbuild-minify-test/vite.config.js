24975756c027f90805c6b048584aabee92719e44
Kamil Skalski <kamil.skalski@gmail.com>
2022-05-10 17:00:00 +0200

    Add package lock changes and config experiment.

--- esbuild-minify-test/vite.config.js
+++ esbuild-minify-test/vite.config.js
@@ -3,6 +3,9 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    minifySyntax: false
+  },
   esbuild: {
     minify: false,
     minifySyntax: false
